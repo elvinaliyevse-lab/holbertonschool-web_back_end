f31432c11ba58c3d7d5d16fb858f118b5c481449
elvinaliyevse-lab <elvin.aliyev.se@gmail.com>
2026-08-30 22:15:24 +0400

    a

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -9,29 +9,22 @@ PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}}, {"$limit": 10}]
 
 
-def log_stats(mongo_collection, option=None):
+def log_stats(mongo_collection):
     """ script that provides some stats about Nginx logs stored in MongoDB
     """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    total = mongo_collection.count_documents({})
+    print(f"{total} logs")
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    status_check = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
     print("IPs:")
-
     for ip in mongo_collection.aggregate(PIPE):
         print(f"\t{ip.get('_id')}: {ip.get('count')}")
 
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    log_stats(MongoClient('mongodb://127.0.0.1:27017').logs.nginx)
