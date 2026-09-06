b7267fa8b9057177f7de18526a17475def31222a
elvinaliyevse-lab <elvin.aliyev.se@gmail.com>
2026-09-06 21:06:07 +0400

    x

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Module of Auth class
+"""
+from typing import List, TypeVar
+
+from flask import request
+
+
+class Auth:
+    """ Template for all authentication systems
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determine if a path requires authentication
+
+        Return:
+          - True if path is not in excluded_paths, False otherwise
+          - the comparison is slash tolerant
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Return the value of the Authorization header of the request
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return the User instance linked to the request
+        """
+        return None
